413f09e9a2d466340b60578b55dd9c913c0b5f52
Layerus <al45677@un.edu.mx>
2026-03-31 10:41:50 -0600

    logic fix

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import random
 operadores = [1,2,3,4,5,6]
 # 1 = suma|2 = resta|3 = divison|4 = multiplicacion|5 = raiz|6 = exponente
 
-print("Facil - 1\nIntermedio - 2\nDificil - 3\n")
+print("Facil - 1\nIntermedio - 2\nDificil - 3\nUniversidad - 4\n")
 while True:
-    seleccion1 = int(input("Seleccione el nivel de dificultad\n "))
-    if seleccion1 < 1 or seleccion1 > 3:
+    seleccion1 = int(input("Seleccione el nivel de dificultad\n"))
+    if seleccion1 < 1 or seleccion1 > 4:
         print("Seleccione un numero valido")
         
     elif seleccion1 == 1:
-        print("Facil")
+        print("Primaria")
         part1 = random.randint(1,50)
         part2 = random.randint(1,50)
 
@@ -19,25 +19,35 @@ while True:
             operadoresAux = random.choice(operadores)
         if operadoresAux == 1:
             simbolo = "+"
+            part1 = random.randint(1,100)
+            part2 = random.randint(1,100)
             correct = part1 + part2
         elif operadoresAux == 2:
             simbolo = "-"
+            part1 = random.randint(1,100)
+            part2 = random.randint(1,100)
             correct = part1 - part2
         elif operadoresAux == 3:
             simbolo = "/"
+            part1 = random.randint(1,20)
+            part2 = random.randint(1,20)
             correct = round(part1 / part2, 2)
         elif operadoresAux == 4:
             simbolo = "*"
+            part1 = random.randint(1,10)
+            part2 = random.randint(1,10)
             correct = part1*part2
             
         print(f"{part1}{simbolo}{part2}")   
-        result = float(input("Indique el resultado"))
+        result = float(input("Indique el resultado\n"))
         if result == correct:
             print("correcto!")
         else:
             print("incorrecto!")
         
     elif seleccion1 == 2:
-        print("Intermedio")
+        print("Secundaria")
+    elif seleccion1 == 3:
+        print("preparatoria")
     else:
-        print("Dificil")
+        print("universidad")
